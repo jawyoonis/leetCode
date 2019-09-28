fd7e4662c0483532b1e2a4d3403f6c257ad6c4be
jawyoonis <jamaalmohammad123@gmail.com>
2019-09-28 14:53:29 -0400

    solved first

---
+++ trap#42.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def trap(self, arr):
+        water=0
+        left, right= 0, len(arr)-1
+        leftwall, rightwall= 0, 0
+        while left < right:
+            if arr[left] < arr[right]:
+                if leftwall < arr[left]:
+                    leftwall= arr[left]
+
+                else:
+                    water+=leftwall-arr[left]
+                    left+=1
+            else:
+                if rightwall < arr[right]:
+                    rightwall= arr[left]
+                else:
+                    water+=rightwall-arr[right]
+                    right-=1
+        return water
+
+
+arr=[0,1,0,2,1,0,1,3,2,1,2,1]
+print(Solution().trap(arr))
