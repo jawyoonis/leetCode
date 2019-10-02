b9192d7408a33f34bc54aa2546374f9a016d6f88
jawyoonis <jamaalmohammad123@gmail.com>
2019-10-01 21:18:57 -0400

    fizzbuzz problem

---
+++ fizzBuzz.py
@@ -0,0 +1,25 @@
+# if the number is multiply of 3 return "fizz". If it is multiple of five return "Buzz" 
+# if the number is mutliple both five and three then return "fizzBuzz"
+
+
+def fizzBuzz(n):
+    result=[]
+    for i in range(1, n+1):
+        if i % 3==0:
+            result.append("Fizz")
+        elif i % 5==0:
+            result.append("Buzz")
+        elif i % 15==0:
+            result.append("FizzBuzz")
+        else:
+            result.append(str(i))
+    return result 
+
+
+
+
+
+
+
+
+print(fizzBuzz(15))
